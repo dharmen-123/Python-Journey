7c8cb00f1d68d6fdcdce4706b51b01a4eab5cfab
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-09 21:01:51 +0530

    commit

---
+++ identityOP.py
@@ -0,0 +1,20 @@
+#  Identity Operators
+# is
+# is not
+x=10
+y="10"
+# print(x is y)
+# print(x==y)
+# print(x is not y)
+# print(bin(x))
+# print(oct(x))
+# print(hex(x))
+n=0B10
+n1=0b10
+# print(n,n1)
+n2=0o101
+n3=0O101
+# print(n2,n3)
+x=10
+y=15
+print(x & y)
