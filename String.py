bf2aaf1733be973150374fc6b01e55a706b5b05a
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-10 18:10:46 +0530

    new update

--- String.py
+++ String.py
@@ -5,3 +5,18 @@
 # String 
 # list
 # tuple
+    
+    #  STRING 
+# +ve                                   -ve 
+# start with -1                    start with 0
+# read direction R->L         read direction L->R
+# write direction L->R         write direction L->R 
+
+str = "Python"
+# print(str[1:5])         
+# print(str[1:])         
+# print(str[-1])         
+# print(str[:-1])         
+# print(str[:-1:])         
+# print(str[1:6:2]) 
+print(str.index('t'))        
