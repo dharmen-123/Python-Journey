2bc86ea7fcab64c54c629e17681eb39291b54eee
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-20 17:48:57 +0530

    commit

---
+++ indexing.py
@@ -0,0 +1,7 @@
+# indexing is use for accesssing the elements of ordered collection 
+lst=[228,33.33,'A',"dharmendra"]
+tpl=(98,'D',"dharmendra",9.87)
+
+print(lst.index(228))
+print(tpl[2])
+print(lst[2])
