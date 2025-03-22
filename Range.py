b329f0c179de2e6876ca3f311649d7a42bbd9fef
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-22 18:08:07 +0530

    update

--- Range.py
+++ Range.py
@@ -3,8 +3,21 @@
 # x=range(1,7)
 # print(list(x))
 # print(tuple(x))
-t=range(2,21,2)
-print(list(t))
-n=int(input("Enter the number: "))/
+
+        # PRINT NATURAL NUMBER
+# n=int(input("Enter the number: "))
 # n1=range(1,n+1)
 # print(list(n1))
+         #PRINT EVEN NUMBER
+# n=int(input("Enter the number: "))
+# t=range(2,21,2)
+# print(list(t))
+
+#  PRINT NEGATIVE NUMBER
+# nve=range(-1,-7,-1)
+# print(list(nve))
+
+c1=range(7,0,-2)
+print(list(c1))
+c2=range(-3,4)
+print(list(c2))
