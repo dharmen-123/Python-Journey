90f3a98ea271d66fd8e446d432d4e0b035db7d2b
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-24 18:21:25 +0530

    commit

--- DataType.py
+++ DataType.py
@@ -13,7 +13,69 @@
 # 3.unordered collection - set and frozenset
 # 4.sequential / Ordered - string ,list , tuple
 
-x=10
-y=20
-z=x+y
-print(type(z))
+
+########  NUMERIC DATA TYPE ############
+
+
+
+# x=10
+# y=20
+# z=x+y  # Addition
+# a=x-y  # Subtraction
+# b=x*y   #Multiplication
+# c=x/y     #Division
+# d=x%y    #Modulus
+# e=x//y    #Floor division
+# print(type(z))
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(id(z))
+ 
+ ##########  FLOAT ##############
+
+# r=2.3
+# b=6.3
+# print(type(r/b))
+
+############ COMPLEX ############
+# s = 3+8j
+# print(type(s))
+
+################ STRING DATA TYPE ####################
+'''
+ * Collection of character
+ * representation in 
+       ' ' single line  string
+       " " single line  string
+       ''' '''multiline string
+ * also called ordered collection
+ * indexing and slicing suppport      
+        '''
+a1=str()
+a2="dharmendra"
+print(type(a2))
+print(str(a2))
+
+#  function call directly
+# Method call when object is created 
+############# METHODS IN PYTHON ##################
+# lower()
+# title()
+# joint() imp
+# split() imp
+# count()
+# capitalize()
+# upper()
+# swapcase()
+
+s= "I am Dharmendra"
+print(s.lower())
+print(s.upper())
+print(s.capitalize())
+print(s.title())
+print(s.swapcase())
+# print(s.count())
+print(s.index())
