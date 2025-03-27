b909e3108e1dc3a7632a3134c2026ab6ca7082ad
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-27 18:06:47 +0530

    commit

--- listdtype.py
+++ listdtype.py
@@ -1,10 +1,22 @@
 li1 =[2,3,4,4]
-
 # sort(reversed-True) 
 li1.append('dharmendra')
 li1.clear()
 # print(li1)
 l=[10,20,'A']
 x=l.copy()
-print(id(l))
-print(id(x))
+# print(id(l))
+# print(id(x))
+
+# print(l.count(10))
+l.extend([10,20,6])
+# print(l)
+
+
+# print(l.index('A'))
+li=[2,3,4,4]
+l.extend(li)
+# print(l)
+# print(l.index('A'))
+l.insert(3,'hello')
+print(l)
