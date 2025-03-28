3ebddebb5a934d991eaac26627f5729b5d4d5d2d
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-28 17:39:08 +0530

    commit

---
+++ tupledtype.py
@@ -0,0 +1,10 @@
+# Collection of homogeneous and hetrogeneous elements 
+# represented as () with comma seperated 
+# ordered collection 
+# indexing and slicing are supported 
+# immutable in nature 
+# It does not change the structure  
+   
+    # there are only two methods 
+    # index and count 
+    
