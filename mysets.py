7f951594b0d58f9b4b76bd907054c7a1bf98d740
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-31 17:51:06 +0530

    change

--- mysets.py
+++ mysets.py
@@ -6,17 +6,28 @@
 # indexing and slicing are not supported
 # Muttable in nature  
 
-  
+  ### function #####
+#len()
+#max()
+# min()
+# type()
+# sum()
+# id()
+
 
 st1={'dharmendra','AIML','Btech',29827,9.5}
-print(type(st1))
+# print(st1)
+# print(len(st1))
+# print(type(st1))
+# print(max(st1))
+# print(id(st1))
 st1.add(10)
 st2=st1.copy()
 # print(st2)
-st1.clear()
+# st1.clear()
 st2.remove('Btech')
 # print(st2)
 
 # print(st2.pop())
 st2.update('A')
-print(st2)
+# print(st2)
