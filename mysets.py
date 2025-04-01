f6bb726d096a49948de06a9ee61097c06c752a22
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-01 17:40:44 +0530

    commmit

--- mysets.py
+++ mysets.py
@@ -43,4 +43,8 @@ st2.discard('aiml')
 print(st1)
 s1={1,2,3,4,5,6}
 s2={4,5,6,7,8,9}
-print(s1.union(s2))
+# print(s1.union(s2))
+print(s1.intersection(s2))
+# s1.intersection_update(s2)
+s1.difference_update(s2)
+print(s1)
