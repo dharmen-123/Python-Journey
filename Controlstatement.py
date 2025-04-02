6993728b25a7d237e2d274bd1e6e2a7be36243dd
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-02 18:10:08 +0530

    commit

---
+++ Controlstatement.py
@@ -0,0 +1,17 @@
+#  CONTROL STATEMENT IS  DIVIDED INTO THREE PARTS 
+# 1 Conditional statement    2 Iterative statement          3 transfer statement 
+    # |-if                        |-while loop                    |-continue
+    # |-if else                   |-for loop                      |-pass
+    # |-if elif                                                   |-break
+    # |-if elif else              
+
+
+# x=10
+# y=20
+# if x>y:
+#     print("X is greater  then y")
+
+a=int(input("Enter the Number : "))
+if a%2==0:
+    print(f"Given number {a} is Even number")
+    
