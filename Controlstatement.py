5bb8c722942adb1090374132a8ab79e6066cd87a
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-03 18:05:59 +0530

    changes

--- Controlstatement.py
+++ Controlstatement.py
@@ -13,7 +13,7 @@
 
 # a=int(input("Enter the Number : "))
 # if a%2==0:
-#     print(f"Given number {a} is Even number")
+#    print(f"Given number {a} is Even number")
   
   ###### Add two number at run time input from user ######
 a=int(input("Enter the Number 1: "))
---
+++ task.py
@@ -0,0 +1,77 @@
+   ########### Q.1 SWAP without using 3 variable ########
+# a=int(input("enter the number "))
+# b=int(input("enter the number "))
+# a,b=b,a
+# print(f"The value of a is {a} \nThe value of b is {b}")
+
+
+    ##### Q.2  SWAP using third variable#############
+# a=int(input("enter the number "))
+# b=int(input("enter the number "))
+# c=a
+# a=b
+# b=c
+# print(f"The value of a is {a} \nThe value of b is {b}")
+
+     ######## Q.3 SWAP using add/sub  #######
+# x=int(input("enter the number "))
+# y=int(input("enter the number "))
+# x=x+y
+# y=x-y
+# x=x-y
+# print(f"The value of x is {x} \nThe value of y is {y}")
+
+
+   ######## Q.4 Area of circle  #############
+
+# r=int(input("enter the radius of circle: "))
+# print(f"The Area of circle is {3.14*r**2}")
+ 
+    #### Q.5 ID and Type of value  ########
+
+# num=int(input("Enter the number : "))
+# print(f"The id of {num} is {id(num)} \nThe type of {num} is {type(num)}")
+
+   ######### Q.6 Sqaure of number ########
+
+# sr=int(input("enter the number for Square "))
+# print(f"The Square of {sr} is {sr*sr}")
+ 
+  ######## Q.7 Square root of number ######
+
+# sqr=int(input("enter the number for Sqr root "))
+# print(f"The Square of {sqr} is {sqr**.5}")
+
+   ######### Q.8 Cube root of number ######
+
+# cub=int(input("Enter the number for cube "))
+# print(f"The cube of {cub} is {cub**3}")
+
+############ Q.9 Cube root of the number #######
+# cr=int(input("Enter the number "))
+# print(f'The cube root of {cr} is {cr**(1/3)}')
+
+  ######## Q.10 Area of square #######
+
+# x=int(input("enter the number "))
+# print(f"The area of Square of side {x} is {x*x}")
+
+   ### Q.11 Area of Rectangle ########
+# a=int(input("enter the length "))
+# b=int(input("enter the width "))
+# print(f"The area of rectangle of length{a} and width{b} is {a*b}")
+
+  ####### Q.12 Area of Traingle ########
+
+# a=int(input("enter the height "))
+# b=int(input("enter the base "))
+# print(f"The area of triangle is {(a*b)/2}")
+
+
+
+# a=int(input("Enter the num "))
+# if(a%2==0):
+#     print(f"The num is even")
+# else :
+#     print(f"the num is odd")
+
