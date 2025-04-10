ad8abb748b471a350c9bf36bbb7de74b8e753b38
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-10 18:25:30 +0530

    update

--- Patterns.py
+++ Patterns.py
@@ -1,5 +1,24 @@
-n=int(input("Enter "))
+     ######### Square Pattern ########
+# n=int(input("Enter "))
+# i=1
+# while(i<=n):
+#     print(' * '*n)
+#     i=i+1
+
+   
+# n=int(input("Enter the number : "))
+# for i in range(1,n+1):
+#     print(' * '*n)
+ 
+ ############# Right Angle Triangle ###########
+# n=int(input("Enter the number : "))
+# i=1
+# while(i<=n):
+#     print('   '*(n-i)+' * '*i)
+#     i=i+1 
+
+n=int(input("Enter the number : "))
 i=1
 while(i<=n):
-    print(' * '*n)
-    i=i+1
+    print(' * '*i+'   '*(n-i))
+    i=i+1 
