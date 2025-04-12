63020be519f6d205646392ed13768d581561f413
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-12 17:41:28 +0530

    commit

--- Patterns.py
+++ Patterns.py
@@ -130,12 +130,22 @@
 #     i=i+1 
 
 
-n=int(input("Enter the number : "))
-i=1
-while(i<=n):
-    print('*'*i+' '*(n-i))
-    i=i+1
-i=1
-while(i<n):
-    n=n-1
-    print('*'*n)
+# n=int(input("Enter the number : "))
+# i=1
+# while(i<=n):
+#     print('*'*i+' '*(n-i))
+#     i=i+1
+# i=1
+# while(i<n):
+#     n=n-1
+#     print('*'*n)
+
+n = int(input("Enter Your Number : "))
+i = 1
+while i < n+1:
+    print("* " * i)
+    i += 1
+num = n - 1
+while num > 0:
+    print("* " * num)
+    num = num - 1
