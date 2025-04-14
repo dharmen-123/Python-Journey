bd3d6fa171ece66421635e57b5d09d19bc671855
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-14 17:35:49 +0530

    commit

--- Patterns.py
+++ Patterns.py
@@ -182,3 +182,13 @@
 # while(i<n):
 #     print(' '*i+' *'*(n-i))
 #     i=i+1 
+
+n=5
+i=0
+while(i<=n):
+  print(" *"*(n-i)+" "*i)
+  i=i+1
+i=1
+while(i<n):
+  i=i+1
+  print(" *"*i+" "*(n-i))
--- questionfor.py
+++ questionfor.py
@@ -5,8 +5,15 @@
 #         print(i,end=' ')
 
   ########## Character to ASCCI value #############
-s=input("Enter any String : ")
-for i in s:
-    # print(i)
-    print(ord(i)+5)
-    # print(i,ord(i))
+# s=input("Enter any String : ")
+# for i in s:
+#     # print(i)
+#     # print(ord(i)+5)
+#     # print(i,ord(i))
+
+#     ####### ASCCI to Character #########
+    
+#     print(chr(ord(i)+5),end='')
+
+x= eval(input("Enter the data "))
+print(x)
