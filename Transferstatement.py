74608ab763545dfae163ecce4d47781c5310786c
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-15 17:55:34 +0530

    commit

--- Transferstatement.py
+++ Transferstatement.py
@@ -4,3 +4,17 @@
 # # pass 
 # # continue
 
+# break exit the loop 
+
+w=int(input("Enter the number :")) 
+x=int(input("Enter the number :")) 
+y=int(input("Enter the number :")) 
+z=int(input("Enter the number :")) 
+h=max(w,x,y,z)
+print(h)
+while(True):
+    if h%w==0 and h%x==0 and h%y==0 and h%z==0:
+        lcm = h
+        break
+    h=h+1
+print(f"The LCM of given number is {lcm}")
