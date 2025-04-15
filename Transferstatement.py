a885e3fd9899b70b4d8ddacf80f5b7eeb940e283
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-15 18:25:46 +0530

    commit

--- Transferstatement.py
+++ Transferstatement.py
@@ -6,15 +6,26 @@
 
 # break exit the loop 
 
-w=int(input("Enter the number :")) 
-x=int(input("Enter the number :")) 
-y=int(input("Enter the number :")) 
-z=int(input("Enter the number :")) 
-h=max(w,x,y,z)
-print(h)
-while(True):
-    if h%w==0 and h%x==0 and h%y==0 and h%z==0:
-        lcm = h
-        break
-    h=h+1
-print(f"The LCM of given number is {lcm}")
+   ################ LCM of ANY NUMBER ############
+   
+# w=int(input("Enter the number :")) 
+# x=int(input("Enter the number :")) 
+# y=int(input("Enter the number :")) 
+# z=int(input("Enter the number :")) 
+# h=max(w,x,y,z)
+# print(h)
+# while(True):
+#     if h%w==0 and h%x==0 and h%y==0 and h%z==0:
+#         lcm = h
+#         break
+#     h=h+1
+# print(f"The LCM of given number is {h}")
+
+
+
+  ################# Pass Skip the block #######
+  ################# Continue Skip the iteration  #######
+
+n=10
+i=0
+  
