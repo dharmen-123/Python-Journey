832c36e3a7409d9256583a07020fccb418031eed
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-16 18:08:39 +0530

    changes

--- Functionin.py
+++ Functionin.py
@@ -1,1 +1,14 @@
 #  Function is use for code reusability 
+ ###### SYNTAX of funtion #########
+# def Function_name (parameter):  #function initialize
+#     |
+#     |funnction_body           #function declaration
+#     |
+#     |return Value 
+
+# Function_name(argument) #function calling
+
+def Add(x,y):
+    print(x+y)
+print(Add(10,5))
+Add(5,8)
