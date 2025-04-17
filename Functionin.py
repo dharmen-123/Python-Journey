bd6ba4e82dbe7c41efde037f83eb4010bc4c3db8
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-17 17:39:29 +0530

    commmit

--- Functionin.py
+++ Functionin.py
@@ -8,8 +8,29 @@
 
 # Function_name(argument) #function calling
 
+
 # def Add(x,y):
 #     print(x+y)
 # print(Add(10,5))
 # Add(5,8)
+
+# def Add(x,y):
+#     return(x+y)
+# print(Add(10,5))
+# Add(5,8)
+
+# def Add(x,y):
+#     print(x+y)
+# z=Add(10,5)
+# print(z)
+# print("Hello")
+# print(z)
+
+
+def Even(n):
+    for i in range(1,n+1):
+        if(i%2==0):
+            print(i)
+n=int(input("Enter the number :"))
+Even(n)
 
