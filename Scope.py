8e406b8c968d89347081bedb754fb88caf3838ca
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-19 18:06:53 +0530

    changes

--- Scope.py
+++ Scope.py
@@ -1,14 +1,20 @@
-
+    ########## SCOPE OF VARIABLES ###
 
 # x=10
+# print(id(x))
 # if x:
 #     x=20
 #     print(x)
+#     print(id(x))
 # print(x)
 
+       #########################
+
 x=10
+print(id(x))
 def display():
     x=20
     print(x)
+    print(id(x))
 display()
 print(x)
