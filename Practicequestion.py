33e1b7b703915e283ef39e2b64b2e3c2d052816e
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-27 21:54:59 +0530

    update

--- Practicequestion.py
+++ Practicequestion.py
@@ -20,3 +20,20 @@
 # while(i<=(2*n)):
 #      print(" "*(2*n-i)+" *"*i)  
 #      i=i+2
+
+
+######################################### 
+
+# s="i Am dharmendra Chilhate"
+# print(s.capitalize())
+# print(s.count('a'))
+# print(s.lower())
+# print(s.upper())
+# print(s.title())
+# print(s.isalnum())
+# print(s.replace('i','My'))
+
+# v=['a','e','i','o','u']
+v="aeiou"
+s='dharmendra'
+print(s in v)
