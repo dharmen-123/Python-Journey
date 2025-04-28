62e97a8fcf1dcbcc33e1c2cf95e4712f3f0e5c21
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-28 11:24:49 +0530

    commit

--- Practicequestion.py
+++ Practicequestion.py
@@ -33,7 +33,47 @@
 # print(s.isalnum())
 # print(s.replace('i','My'))
 
-# v=['a','e','i','o','u']
-v="aeiou"
-s='dharmendra'
-print(s in v)
+          ########################################
+
+# s=input("Enter the String : ").lower()  
+# vowel="aeiou"
+# v=0
+# c=0
+# for i in s:
+#     if i in vowel:
+#         v+=1
+#     else:
+#        i.isalpha()
+#        c+=1
+# print(f"Vowel is {v} and Consonants is {c}")
+
+
+   ############## COUNT THE DIGIT SMALL CAPITAL LETTER #############
+
+# val=input("Enter the string : ")
+# capital=0
+# small=0
+# num=0
+# for i in val:
+#     if i.isdigit():
+#         num+=1
+#     elif i.islower():
+#         small+=1
+#     else:
+#         capital+=1
+# print(capital , small , num)
+
+# s1="python"
+# s2="thyonp"
+# c1=0
+# c2=0
+# for i in s1:
+#     if i in s2:
+#         c1+=1 
+#         c1-=1 
+# print(c1)
+
+
+# input_number  = 5
+# for i in range(input_number):
+#     print(" " * (input_number - i) + "*" * (2*i + 1))
