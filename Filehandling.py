9e5f132a4569a3ad706224a0628985072ce45f3a
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-05-01 18:06:46 +0530

    commit

---
+++ Filehandling.py
@@ -0,0 +1,12 @@
+  ############### FILE HANDLING ############
+    ## store data permanently ##
+    ### Operations 
+    #   open()
+    #   read/write()
+    #   close()
+
+#in w mode cursor position is start from zero
+#x mode create new file
+
+f=open('n4.py', 'x')
+print("File created")
