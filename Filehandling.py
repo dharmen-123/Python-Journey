46c6a8e46dcea81d9f72da39c96dc3de99ca4dde
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-05-01 18:08:59 +0530

    changes

--- Filehandling.py
+++ Filehandling.py
@@ -6,7 +6,15 @@
     #   close()
 
 #in w mode cursor position is start from zero
-#x mode create new file
-
-f=open('n4.py', 'x')
+# x mode create new file
+# cursor position in x mode is zero
+f=open('n4.py', 'w')
 print("File created")
+print(f.name)
+print(f.mode)
+print(f.readable)
+print(f.writable)
+print(f.closed)
+f.close
+print(f.closed)
+
