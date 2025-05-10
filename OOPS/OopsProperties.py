4656c45be32287219ad62a6898fc10a8de309d48
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-05-10 17:58:43 +0530

    dict

---
+++ OOPS/OopsProperties.py
@@ -0,0 +1,26 @@
+# Properties of OOPs
+
+##   1 Inheritence
+##   2 Polymorphishm
+##   3 abstraction
+##   4 Encapsulation
+
+# Encapsulation is defined as the wrrapup variables and method into a single unit
+
+# class Python:
+#     school= "SWS"
+#     def __init__(self , name):
+#         self.name=name
+#     def show_details(self):
+#         print(self.name)
+#         print(Python.school)
+
+# obj=Python("Dharmendra")
+
+class First:
+    "for demo purpose hbuhbjmuihbuuggvcybugsuttbn"
+    pass
+print(len(dir(First)))
+print(First.__module__)
+print(First.__doc__)
+print(First.__dict__)
