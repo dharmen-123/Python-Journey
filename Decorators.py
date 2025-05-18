8b4c1a5e3bf957faf1f06b2a30e842c403eaae51
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-05-18 18:16:27 +0530

    decorator

--- Decorators.py
+++ Decorators.py
@@ -1,9 +1,9 @@
  ######### decorator is used to modify and change the behaviour of other function without changing its code
 
 # def decorator(fun): 
-
+#     fun()
 #     return 5
-
+    
 # def main_fun():
 #     print("main function")
 # x=decorator(main_fun)
@@ -46,15 +46,29 @@
 # b=int(input("Enter the number :"))
 # Mainfun(a,b)
 
-def outer_func(main_func):
-    def inner_func(s,t):
-        s=s*2
-        t=t+2
-        main_func(s,t)
-    return inner_func   
-@outer_func
-def main_func(p,q):
-    print(p+q)
-s=int(input("Enter any number :"))
-t=int(input("Enter any number :")) 
-main_func(s,t)
+
+# def outer_func(main_func):
+#     def inner_func(s,t):
+#         s=s*2
+#         t=t+2
+#         main_func(s,t)
+#     return inner_func   
+# @outer_func
+# def main_func(p,q):
+#     print(p+q)
+# s=int(input("Enter any number :"))
+# t=int(input("Enter any number :")) 
+# main_func(s,t)
+
+##################################
+def Special(fun):
+    def Inside(num):
+       num=num+1
+       fun(num)
+    return Inside
+@Special
+def Main(n):
+    for i in range(1,11):
+        print(n,"X",i,"=",n*i)
+n=int(input("Enter the number for print table :"))
+Main(n)
