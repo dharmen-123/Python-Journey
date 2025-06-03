e0f9be5ad0b4905d79e7dd31a0880ea233126b60
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-06-03 21:18:02 +0530

    commit

--- OOPS/Instancevariable.py
+++ OOPS/Instancevariable.py
@@ -1,19 +1,19 @@
 
 #### instance variables 
 
-class Student:
-    def __init__(self,name,city):
-         self.x=name    ## Declaration of instance variables inside the class
-         self.y=city    ## Declaration of instance variables inside the class
-    def show(self):
-        print(self.x,self.y)
-obj=Student("dhamendra","BPL")
-obj1=Student("Amit","MDDP")
-obj.z=10       ## Declaration of instance variables outside the class
-print(obj.z)
-print(obj.x)
-print(obj.y)
-obj.show()
+# class Student:
+#     def __init__(self,name,city):
+#          self.x=name    ## Declaration of instance variables inside the class
+#          self.y=city    ## Declaration of instance variables inside the class
+#     def show(self):
+#         print(self.x,self.y)
+# obj=Student("dhamendra","BPL")
+# obj1=Student("Amit","MDDP")
+# obj.z=10       ## Declaration of instance variables outside the class
+# print(obj.z)
+# print(obj.x)
+# print(obj.y)
+# obj.show()
 
 
 # If you want access and declare the instance variable inside the class self keyword is used 
