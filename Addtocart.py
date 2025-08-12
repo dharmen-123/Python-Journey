3345fbe7a62c7727efd5e5e9ec7fc4af8647f653
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-08-12 23:41:40 +0530

    changes

--- Addtocart.py
+++ Addtocart.py
@@ -5,4 +5,9 @@ cart={}
 cart['userid']=1
 cart['items']=[]
 cart['quantity']=[]
+cart['items'].append(2)
+cart['quantity'].append(3)
+print(cart)
+cart['items'].append(6)
+cart['quantity'].append(1)
 print(cart)
