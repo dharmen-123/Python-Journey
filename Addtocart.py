8698efcd359d5b2d40be49f945c67799dcdfb7aa
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-08-16 18:36:49 +0530

    commit

--- Addtocart.py
+++ Addtocart.py
@@ -1,19 +1,24 @@
 
 # ############### Add to cart in Dictionary Format ##########
 
-# cart={}
-# cart['userid']=1
-# cart['items']=[]
-# cart['quantity']=[]
-# cart['items'].append(2)
-# cart['quantity'].append(3)
-# print(cart)
-# cart['items'].append(6)
-# cart['items'].append(3)
-# cart['quantity'].append(1)
-# cart['quantity'].append(4)
-# print(cart)
-# item_index=cart['items'].index(3)
-# print(item_index)
-# item_qua=cart['quantity'][item_index]
-# print(item_qua)
+cart={}
+cart['userid']=1
+cart['items']=[]
+cart['quantity']=[]
+cart['items'].append(2)
+cart['quantity'].append(3)
+print(cart)
+cart['items'].append(6)
+cart['items'].append(3)
+cart['quantity'].append(1)
+cart['quantity'].append(4)
+print(cart)
+item_index=cart['items'].index(3)
+print(item_index)
+item_qua=cart['quantity'][item_index]
+print(item_qua)
+
+
+a=(999*89)/100
+
+print(int(a))
