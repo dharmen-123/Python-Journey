c28e5f499b6f2ba0389966b393fc0d1910681890
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-08-18 12:15:30 +0530

    commit

--- Addtocart.py
+++ Addtocart.py
@@ -13,13 +13,17 @@ cart['items'].append(3)
 cart['quantity'].append(1)
 cart['quantity'].append(4)
 print(cart)
+if 3 not in cart['items']:
+    print("This item is not present")
+else :
+    print("This is present")
 item_index=cart['items'].index(3)
 print(item_index)
 item_qua=cart['quantity'][item_index]
 print(item_qua)
 
 
-a=int((499*77)/100)
-sp=int(499-a)
-print(a)
-print(sp)
+# a=int((499*77)/100)
+# sp=int(499-a)
+# print(a)
+# print(sp)
