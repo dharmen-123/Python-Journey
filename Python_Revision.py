56698506bdd7daddbd8d6064bb9fa3860ffea9ac
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-06-20 21:03:00 +0530

    commit

---
+++ Python_Revision.py
@@ -0,0 +1,33 @@
+        #   PYTHON REVISION     #
+"""
+# Python is the popullar programming language
+# It is created by the Guido van Rossum in 1991
+# used for web dev (server side application)
+software development
+machine learning 
+AI and Automation 
+
+"""
+# print("Hello Python ")
+
+# for check the python version of editor we can find it by importing the sys module
+# import sys
+# print(sys.version)
+
+# Run the python file using Terminal 
+# Syntax to run the file - "python file_name.py"
+
+        # PYTHON INDENTATION #
+# Indentation refers to the spaces at the beginning of a code line.
+# Python uses indentation to indicate a block of code.
+
+        # PYTHON VARIABLE #
+# in python varibale is created to assign the value
+
+x=10
+name="Ram"
+greet="Good Morning"
+print(x)
+print(name," ",greet)
+
+
